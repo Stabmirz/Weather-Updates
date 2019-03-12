5166dce623cc3b2c488a53789173b569ff858eb6
Mirza <mirza@Mirzas-MacBook-Pro.local>
2019-03-12 00:29:53 -0400

    first commit

---
+++ precache-manifest.083da337a4f86f3991aedc5d42d73bff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6056a4ed2f1454c2d606",
+    "url": "/Weather-Updates/static/css/main.26a29272.chunk.css"
+  },
+  {
+    "revision": "6056a4ed2f1454c2d606",
+    "url": "/Weather-Updates/static/js/main.75a248f6.chunk.js"
+  },
+  {
+    "revision": "4f7fc858a93f2f2e19dd",
+    "url": "/Weather-Updates/static/js/runtime~main.e38fdbc9.js"
+  },
+  {
+    "revision": "3e40b3748f724ffc6a8b",
+    "url": "/Weather-Updates/static/js/2.93bdb068.chunk.js"
+  },
+  {
+    "revision": "f5d531f8475ec7c46a40925f8df5e409",
+    "url": "/Weather-Updates/static/media/weather-background.f5d531f8.jpg"
+  },
+  {
+    "revision": "d40f11099268ff5b4004329366438b59",
+    "url": "/Weather-Updates/index.html"
+  }
+];
